9766590b6da730a2be6a58dc44489495957e58d1
inferno0230 <mail@inferno0230.in>
2025-09-24 13:17:32 +0530

    aston: Update from CPH2609_15.0.0.860(EX01)
    Change-Id: I4c50832d5c69b3d98d6a7f915092fef33c4d19ec
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- Android.mk
+++ Android.mk
@@ -3,31 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),aston)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,98d2aa1630c8d4d5988d89c210945ff75f930b67)
-$(call add-radio-file-sha1-checked,radio/aop.img,f3ebd81bd714779f30a62597ab6d94b54c72b90a)
-$(call add-radio-file-sha1-checked,radio/aop_config.img,aaa0f4adf2b9f60e17258756898f07276c83c72a)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,b8e8a62df4ccd5a6f2113efc5c062e44d406c777)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,01138b6fb86d110a27fb7a7079ebcb97e393c325)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,c535cac213a6bf404edb7ae6505349e498e40947)
-$(call add-radio-file-sha1-checked,radio/dsp.img,28885c51ced35d72cb8f10a1e250fe7a8c427bab)
-$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,13f5f33df779408b04cdfb3d78dd1ebbd3afb7b4)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,47c7f2d80871f9d340c01b73c4c1ee13147e1cab)
-$(call add-radio-file-sha1-checked,radio/hyp.img,a208683510b33e07ef84ec188413caa9cae71637)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,a2a882339a037ba4877e5ca988cb4f182fdeed82)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,a2ecf20e14d286ad7e0bd466d187a505d2c8940b)
-$(call add-radio-file-sha1-checked,radio/modem.img,c03a1a4985f369dc076a7bba83108617410bc1e0)
-$(call add-radio-file-sha1-checked,radio/oplus_sec.img,1c584c7b98f32a7ccceebbad7564e3bc0944ccb0)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,10fbb47d6e124cc913ffad405753283fe1751b48)
-$(call add-radio-file-sha1-checked,radio/shrm.img,9e84c63f03dd9cdbe443eeefbb76b6c7ee4543bc)
-$(call add-radio-file-sha1-checked,radio/splash.img,0e82d9c1514da572cd55af48928475681fc15797)
-$(call add-radio-file-sha1-checked,radio/tz.img,9ea7d44d910bcfd4c0770a566884c93dbcc9cc0d)
-$(call add-radio-file-sha1-checked,radio/uefi.img,ea77d7adbb5ba151210df02af928b61eef438cb8)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,26dfd5d0bf01d9b72c49f971f677f7f15e703a86)
-$(call add-radio-file-sha1-checked,radio/xbl.img,205b8557f4a4e11d6a91eb996e96ac6d8444e3ec)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,55fc669497d10f2630f5809e32667ca4b707a75d)
-$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,49591c9bbfceefcf999237fda9bb999968c96331)
-
-endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,28 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    aop_config \
-    bluetooth \
-    cpucp \
-    devcfg \
-    dsp \
-    engineering_cdt \
-    featenabler \
-    hyp \
-    imagefv \
-    keymaster \
-    modem \
-    oplus_sec \
-    qupfw \
-    shrm \
-    splash \
-    tz \
-    uefi \
-    uefisecapp \
-    xbl \
-    xbl_config \
-    xbl_ramdump
