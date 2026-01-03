d35ab16d470dba20ae6deda6b93f244569f05487
Bruno Martins <bgcngm@gmail.com>
2026-01-03 08:28:48 +0530

    aston: Import TFA ADSP modules
    Change-Id: Ibe2bb2f1ee690abaf5208526c48861fea012eb5b
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- aston-vendor.mk
+++ aston-vendor.mk
@@ -1334,6 +1334,8 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.cammidasservice-V1-ndk \
     vendor.oplus.hardware.sendextcamcmd-V1-service-impl \
     vendor.oplus.hardware.sendextcamcmd-V2-ndk \
+    libtfadsp_sb4_0_rx \
+    libtfadsp_sb4_0_tx \
     EuiccGoogle \
     vendor.qti.camera.aon-impl-1.3.xml \
     vendor.qti.camera.postproc-impl.xml \
