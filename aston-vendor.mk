055ec1a1f1830c32b5eaa043fa18586952ea625b
Bruno Martins <bgcngm@gmail.com>
2026-01-12 00:37:50 +0000

    aston: Ship device-specific SE manifest fragment
    This was previously commonized, but it turns out that each target
    declares support for its own instances, not allways the same.
    
    Change-Id: Ic2c1447f5c62b1aab4edbddee07b5c473d38b221

--- aston-vendor.mk
+++ aston-vendor.mk
@@ -1344,5 +1344,6 @@ PRODUCT_PACKAGES += \
     manifest_oplus_cameraextension_aidl.xml \
     manifest_oplus_cammidasservice_aidl.xml \
     manifest_oplus_sendextcamcmd.xml \
+    manifest_secure_element.xml \
     vendor.qti.camera.provider-service_64 \
     vendor.oplus.hardware.cammidasservice-V1-service
