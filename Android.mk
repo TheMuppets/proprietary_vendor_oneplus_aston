fef3bd02a71058e613f07f76448e8f29010a53f1
inferno0230 <mail@inferno0230.in>
2026-01-26 19:24:45 +0530

    aston: Bring back firmware
    Change-Id: Ibb6f6fe2397f509e633a1a2a0c7dd605ccaef4ff

--- Android.mk
+++ Android.mk
@@ -3,3 +3,32 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),aston)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,2c9bbf2eaefdbbf7621a4a2a406a567a306466ec)
+$(call add-radio-file-sha1-checked,radio/aop.img,e7d4a2f862bc9f54cb524495455bbde6c31e647d)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,bca6cc089e63428a3279fdfb7d96f1d3887ebe3b)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,b67b4504fd0bb914e248e1ef91470bb416e5ee6d)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,eabf558e5c2c3db4688f36c6c982380384fd5746)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,d6ada834da4bd244a06f030befad8dff46905ca7)
+$(call add-radio-file-sha1-checked,radio/dsp.img,28885c51ced35d72cb8f10a1e250fe7a8c427bab)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,0afab3c2f5e593506be7c0ee0a249fd4880fa2a1)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,3ad2d3048387ef6d7fb960660e12229fd9fe5af1)
+$(call add-radio-file-sha1-checked,radio/hyp.img,61ac9fbe7889f540ce9444e20496d7f6f8afd7d2)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,e697ad56198358190ee55b6d39f5be3fc4c6d892)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,f7811e8a51a0a9ef5724379854278d7e0f40291d)
+$(call add-radio-file-sha1-checked,radio/modem.img,028f9b19a5d02ca01eda4afd4c70d767d70f8b59)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,1c584c7b98f32a7ccceebbad7564e3bc0944ccb0)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,fdec80720c3c548f0a5125b46c92f1155be0d257)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,dec52f54368e606ebfe79ba0f8c8ac26ffff2cbe)
+$(call add-radio-file-sha1-checked,radio/shrm.img,05aa76d0d694b8e307dc2115c7378ef0aad30c02)
+$(call add-radio-file-sha1-checked,radio/splash.img,0e82d9c1514da572cd55af48928475681fc15797)
+$(call add-radio-file-sha1-checked,radio/tz.img,835aaa4d78eba446c87b5f7d891a2d202dfab591)
+$(call add-radio-file-sha1-checked,radio/uefi.img,93053e4fbc7a6068e9aa6020141d65eb135aae0f)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,4e5b6e565caaf4786280e8443aa403c7a7c33748)
+$(call add-radio-file-sha1-checked,radio/xbl.img,50f957434d1c1197241defaf7433940dbaac65cd)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,3a5405c53d380a47d82bc6f0ff399e9fd6c228a2)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,ff125dd5b59fda269635db5010c574568ad800e4)
+
+endif
